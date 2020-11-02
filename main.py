7edbc22a5f6e03a8be4582afbe6a98029e4b4d3e
William Funkenbusch <williamfunkenbusch@gmail.com>
2020-11-02 18:13:16 -0500

    Added training function and test case in main.py.

--- main.py
+++ main.py
@@ -1,20 +1,47 @@
 import numpy as np
 import matplotlib
-%matplotlib inline
 from sklearn.model_selection import train_test_split
 
 import torch
-from torch.nn import Linear, ReLU, CrossEntropyLoss, Sequential, Conv2d, MaxPool2d, Module, BatchNorm2d, Dropout
+from torch.nn import Linear, ReLU, MSELoss, Sequential, Conv2d, MaxPool2d, Module, BatchNorm2d, Dropout
 from torch.optim import Adam
-# Ligand/Proteins pairs will be converted into [N_samples, 2, max_sequence_size, number_of_encodings]
+
+from protein_cnn import *
+# Ligand/Proteins pairs will be converted into [N_samples, 1, max_sequence_size, number_of_encodings]
 
 def main():
-    # defining the model
-    model = Net()
-    # defining the optimizer
+    n_encodings = 64
+    max_sequence_length = 64
+    iterations = 100
+
+    n_samples = 10
+    x_train = np.random.rand(n_samples, 1, max_sequence_length, n_encodings)
+    y_train = np.zeros((n_samples, 1))
+    for i in range(n_samples):
+        x_train_i = x_train[i, :, :, :].reshape(max_sequence_length, n_encodings)
+        y_train[i] = np.sum(x_train_i[0:10, :])/np.sum(x_train_i[10:, :])
+
+    # Get data and split into training and validation
+    x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size = 0.1)
+    x_train = torch.from_numpy(x_train).float()
+    y_train = torch.from_numpy(y_train).float()
+    x_val = torch.from_numpy(x_val).float()
+    y_val = torch.from_numpy(y_val).float()
+
+    # Model Initialization
+    model = Net(n_encodings, max_sequence_length)
     optimizer = Adam(model.parameters(), lr = 0.1)
-    # defining the loss function
-    criterion = CrossEntropyLoss()
+    loss = MSELoss()
+
+    # Model Training
+    train_losses, val_losses = train(model, optimizer, loss, x_train, y_train, x_val, y_val, iterations)
+
+    plt.figure()
+    plt.plot(range(1, iterations + 1), train_losses, label = 'training loss')
+    plt.plot(range(1, iterations + 1), val_losses, label = 'validation loss')
+    plt.xlabel('Iteration')
+    plt.ylabel('Loss')
+    plt.savefig('Loss.pdf')
 
 if __name__ == '__main__':
     main()
--- protein_cnn.py
+++ protein_cnn.py
@@ -1,31 +1,38 @@
 import numpy as np
+import math
 import matplotlib
-%matplotlib inline
 from sklearn.model_selection import train_test_split
 
 import torch
+from torch.autograd import Variable
 from torch.nn import Linear, ReLU, CrossEntropyLoss, Sequential, Conv2d, MaxPool2d, Module, BatchNorm2d, Dropout
 from torch.optim import Adam
 
+### Need to figure out: padding
+#                       how many layers/what layers
+#                       linear layers output
 class Net(Module):   
     def __init__(self, n_encodings, max_sequence_length):
         super(Net, self).__init__()
 
         self.cnn_layers = Sequential(
-            # Defining a 2D convolution layer
-            Conv2d(1, 16, kernel_size = (3, n_encodings), stride = 1),
-            BatchNorm2d(16),
+            Conv2d(1, 128, kernel_size = (3, 3), stride = 1, padding = 1), #2D convolutional layer
+            BatchNorm2d(128),
             ReLU(inplace = True),
             MaxPool2d(kernel_size = 2, stride = 2),
-            # 2nd 2D convolution layer
-            Conv2d(16, 16, kernel_size = (3, n_encodings), stride = 1, padding = 1),
-            BatchNorm2d(16),
+
+            Conv2d(128, 128, kernel_size = (3, 3), stride = 1, padding = 1),
+            BatchNorm2d(128),
             ReLU(inplace = True),
             MaxPool2d(kernel_size = 2, stride = 2),
-        )
+
+            Conv2d(128, 128, kernel_size = (3, 3), stride = 1, padding = 1),
+            BatchNorm2d(128),
+            ReLU(inplace = True),
+            MaxPool2d(kernel_size = 2, stride = 2))
 
         self.linear_layers = Sequential(
-            Linear(16 * n_encodings * max_sequence_length, 1)
+            Linear(int(128 * n_encodings/8 * max_sequence_length/8), 1),
         )
 
     # Defining the forward pass    
@@ -34,3 +41,36 @@ class Net(Module):
         x = x.view(x.size(0), -1)
         x = self.linear_layers(x)
         return x
+
+def train(model, optimizer, loss, x_train, y_train, x_val, y_val, iterations):
+    model.train()
+    train_losses = []
+    val_losses = []
+
+    # Getting the data
+    x_train, y_train = Variable(x_train), Variable(y_train)
+    x_val, y_val = Variable(x_val), Variable(y_val)
+
+    for iteration in range(iterations):
+        optimizer.zero_grad()
+        
+        # Current model predictions
+        output_train = model(x_train)
+        output_val = model(x_val)
+
+        # Current model losses
+        train_loss = loss(output_train, y_train)
+        val_loss = loss(output_val, y_val)
+
+        train_losses.append(train_loss)
+        val_losses.append(val_loss)
+
+        # Updating model
+        train_loss.backward()
+        optimizer.step()
+        train_loss = train_loss.item()
+        val_loss = val_loss.item()
+        # printing the validation loss
+        print('Epoch : ', iteration + 1, '\t', 'loss :', val_loss)
+
+    return train_losses, val_losses
